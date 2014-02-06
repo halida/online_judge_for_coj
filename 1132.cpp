986855a979674700ef12d71faa5fab9fc6e043b4
halida <linjunhalida@gmail.com>
2014-02-07 04:12:08 +0800

    optimize

--- 1132.cpp
+++ 1132.cpp
@@ -28,6 +28,14 @@ void sepp(int v){
             sep[v].push_back(i);
             vt /= i;
             i = 1;
+
+            if ( ! sep[vt].empty() ){
+                std::vector<int> a=sep[v], b=sep[vt];
+                a.insert(a.end(), b.begin(), b.end());
+                sep[v] = a;
+                sort(sep[v].begin(), sep[v].end());
+                return;
+            }
         }
     }
     if (vt != v) sep[v].push_back(vt);
@@ -39,24 +47,29 @@ int solve(int v){
     if (res[v] != -1) return res[v];
     if (sep[v].empty() && v > 1) sepp(v);
 
-    // printf("sep.size = %d\n", sep[v].size());
-
     if (sep[v].size() <= 0) return 0;
     if (sep[v].size() <= 1) return 1;
+    // printf("sep.size = %d\n", sep[v].size());
 
     int last = sep[v][ sep[v].size()-1 ];
-    int last_count = 1;
+    int last_count = 0;
 
-    int pre = 1;
-    for (int i = 0; i < sep[v].size()-1; ++i)
-        {
-            int c = sep[v][i];
-            if (c != last) {
-                pre *= c;
-            } else {
-                last_count ++;
-            }
-        }
+    int pre = v;
+    while( true ){
+        if (pre % last > 0) break;
+        pre /= last;
+        last_count ++;
+    }
+    
+    // for (int i = 0; i < sep[v].size()-1; ++i)
+    //     {
+    //         int c = sep[v][i];
+    //         if (c != last) {
+    //             pre *= c;
+    //         } else {
+    //             last_count ++;
+    //         }
+    //     }
 
     int pre_result = solve(pre);
     int result = 0;
@@ -85,20 +98,41 @@ int lazy_solve(int v){
     return r;
 };
 
+void check_result(){
+    for (int i = 0; i < MAX_X/10; ++i)
+        {
+            int result = solve(i);
+            // printf("sep %d size %d = ", i, static_cast<int>(sep[i].size()));
+            // for (int j = 0; j < sep[i].size(); ++j)
+            //     {
+            //         printf("%d ", sep[i][j]);
+            //     }
+            // printf(", solve: %d\n", result);
+        }
+}
+
+void pre_sep(){
+    // find parameters
+    int i = 2;
+    int m = std::sqrt(MAX_X);
+    for (int i = 1; i <= m; ++i)
+        {
+            if (sep[i].size() > 1) continue;
+            for (int j = 1; j <= MAX_X/i; ++j)
+                {
+                    if (i == 1 && j == 1) continue;
+                    sep[i*j].push_back(i);
+                }
+        }
+    // find parameter count
+}
+
 int main(int argc, char *argv[])
 {
     std::fill(res, (res + MAX_X + 1), -1);
 
-    // for (int i = 0; i < 1000; ++i)
-    //     {
-    //         int result = solve(i);
-    //         printf("sep %d size %d = ", i, static_cast<int>(sep[i].size()));
-    //         for (int j = 0; j < sep[i].size(); ++j)
-    //             {
-    //                 printf("%d ", sep[i][j]);
-    //             }
-    //         printf(", solve: %d\n", result);
-    //     }
+    // pre_sep();
+    // check_result();
     // return 0;
 
     int c = 0;
@@ -113,7 +147,4 @@ int main(int argc, char *argv[])
         }
     return 0;
 }
-
-
-
 
